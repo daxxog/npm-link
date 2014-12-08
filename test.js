168aad1ab6435eb3291230426c713f57c24e5046
David Volm <david@volminator.com>
2014-12-07 18:04:43 -0600

    test extracting the tar

---
+++ test.js
@@ -0,0 +1,35 @@
+var tar = require('tar'),
+    tmp = require('tmp'),
+    request = require('request'),
+    vows = require('vows'),
+    assert = require('assert');
+
+var extract = function(path, cb) {
+    var extractor = tar.Extract({path: path})
+        .on('error', cb)
+        .on('end', cb);
+
+    request('http://localhost:7778/archive.tar')
+        .on('error', cb)
+        .pipe(extractor);
+};
+
+tmp.dir(function(err, path) {
+    console.log(path + '/npm-link');
+
+    if(!err) {
+        vows.describe('npm-link').addBatch({
+            'when extracting tar archive': {
+                topic: function() {
+                    extract(path, this.callback);
+                },
+                'we don\'t get any errors': function(topic) {
+                    assert.equal(topic, undefined);
+                }
+            }
+        }).run();
+    } else {
+        console.log('error: could not open tmp folder!');
+        console.log(err);
+    }
+});
